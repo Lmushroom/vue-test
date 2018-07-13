743a675281bc0dd9b5c5c1e8eaeee0df4cf5b97f
Lmushroom <zwj10213@126.com>
2018-07-13 17:12:47 +0800

    7月活动

---
+++ src/components/active/Holiday.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="holiday">
+		<div class="gifts_box"></div>
+	</div>
+</template>
+
+<script type="text/javascript">
+	export default{
+		name:'Holiday',
+		data(){
+			return {
+
+			}
+		}
+	}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -37,7 +37,7 @@ export default new Router({
     	component:Own
     },
     {
-      path: '/active/Holiday',
+      path: '/Holiday',
       name:'holiday',
       component:Holiday
     }
